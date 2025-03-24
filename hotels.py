635f22902618a3b36c30103be435f5d3db6e7c1f
Popov Anatolii <popov.anatolii@brands.kz>
2025-03-24 15:51:40 +0500

    Separated pydantic schemas to validate input and output data

--- hotels.py
+++ hotels.py
@@ -1,13 +1,15 @@
 from fastapi import Query, Body
 from fastapi.routing import APIRouter
+from schemas.hotels import Hotel, HotelPATCH
 
 
-router = APIRouter(prefix='/hotels')
+router = APIRouter(prefix='/hotels', tags=['Отели'])
 
 HOTELS = [
     {'id': 1, 'title': 'Sochi', 'name': 'Sochi'},
     {'id': 2, 'title': 'Dubai', 'name': 'Dubai'}
 ]
+
 
 
 @router.get('',
@@ -16,7 +18,7 @@ HOTELS = [
 def get_hotels(
     id: int | None = Query(default=None, description="ID"),
     title: str | None = Query(default=None, description="Hotel name"),
-):
+) -> list[Hotel]:
     hotels_ = []
     for hotel in HOTELS:
         if id and hotel['id'] != id:
@@ -28,13 +30,24 @@ def get_hotels(
 
 
 @router.post('',
-             summary='Добавление отеля.',
+             summary='Добавление отеля',
              description='Добавление отеля в базу данных. Необходимо предоставить title в теле запроса.')
-def create_hotel(title: str = Body(embed=True)):
+def create_hotel(hotel_data: Hotel = Body(
+    openapi_examples={
+        '1': {'summary':  'Сочи', 'value': {
+                'title': 'Отель в Сочи 5 звезд',
+                'name': 'Крутой отель алл ин казинузин'
+        }},
+        '2': {'summary':  'Дубай', 'value': {
+            'title': 'Отель в Дубае 5 звезд',
+            'name': 'Ред и Сон чиз блюа'
+        }}}
+)):
     global HOTELS
     HOTELS.append({
         'id': len(HOTELS) + 1,
-        'title': title
+        'title': hotel_data.title,
+        'name': hotel_data.name
     })
     return {'status': 'ok'}
 
@@ -52,16 +65,15 @@ def delete_hotel(hotel_id: int):
             summary='Полное обновление данных по отелю',
             description='Полное обновление данных по отелю. Необходимые переменные title и name')
 def edit_hotel(hotel_id: int,
-                 title: str = Body(),
-                 name: str = Body()):
+               hotel_data: Hotel):
     global HOTELS
     if not any([hotel['id'] == hotel_id for hotel in HOTELS]):
         return {'error': 'No such hotel id'}
 
     for i, hotel in enumerate(HOTELS):
         if hotel['id'] == hotel_id:
-            HOTELS[i]['title'] = title
-            HOTELS[i]['name'] = name
+            HOTELS[i]['title'] = hotel_data.title
+            HOTELS[i]['name'] = hotel_data.name
             break
     return {'status': 'ok'}
 
@@ -69,19 +81,17 @@ def edit_hotel(hotel_id: int,
 @router.patch('/{hotel_id}', 
            summary='Частичное обновление данных об отеле',
            description='Частично обновляем данные об отеле. Необходимо предоставить значение хотя бы для одной переменной title или name, или сразу оба значения.')
-def part_edit_hotel(hotel_id: int,
-                title: str | None = Body(default=None),
-                name: str | None = Body(default=None)):
+def part_edit_hotel(hotel_id: int, hotel_data: HotelPATCH):
     if not any([hotel['id'] == hotel_id for hotel in HOTELS]):
         return {'error': 'No such hotel id'}
-    if title is None and name is None:
+    if hotel_data.title is None and hotel_data.name is None:
         return {'error': 'You must provide at least 1 value'}
     for i, hotel in enumerate(HOTELS):
         if hotel['id'] == hotel_id:
-            if title:
-                HOTELS[i]['title'] = title
-            if name:
-                HOTELS[i]['name'] = name
+            if hotel_data.title:
+                HOTELS[i]['title'] = hotel_data.title
+            if hotel_data.name:
+                HOTELS[i]['name'] = hotel_data.name
             break
     return {'status': 'ok'}
   
