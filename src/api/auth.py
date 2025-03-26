cb2edfd44864a2b9f16711fafd40e94abc117e1e
Popov Anatolii <popov.anatolii@brands.kz>
2025-03-26 23:33:36 +0500

    Added login endpoint

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter
+from datetime import datetime, timedelta, timezone
+from fastapi import APIRouter, HTTPException
 from passlib.context import CryptContext
+import jwt
+from src.config import settings
 
 from src.schemas.users import UserRequestAdd, UserAdd
 from src.database import async_session_maker
@@ -11,6 +14,17 @@ from src.repositories.users import UsersRepository
 router = APIRouter(prefix='/auth', tags=['Authenticate and Authorize'])
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+SECRET_KEY = settings.SECRET_KEY
+ALGORITHM = settings.ALGORITHM
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
+
+
+def create_access_token(data: dict ) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode |= {"exp": expire} 
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 
 @router.post('/register',
@@ -26,4 +40,16 @@ async def register_user(
         await session.commit()
         
         return {'status': 'ok'}
+
+
+@router.post('/login',
+             summary='',
+             description='')
+async def login_user(data: UserRequestAdd):
+    async with async_session_maker() as session:
+        user = await UsersRepository(session).get_one_or_none(email=data.email)
+        if not user:
+            raise HTTPException(status_code=401, detail='No user with such an email address')
+        access_token = create_access_token({'user_id': user.id})
+        return {'access_token': access_token}
     
