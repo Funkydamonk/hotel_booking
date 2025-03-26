c3190fea08790bfa2af822c2ccbf153919c3e4a7
Popov Anatolii <popov.anatolii@brands.kz>
2025-03-27 00:21:35 +0500

    Refactored auth endpoints and added AuthService

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,34 +1,12 @@
-from datetime import datetime, timedelta, timezone
 from fastapi import APIRouter, HTTPException, Response
-from passlib.context import CryptContext
-import jwt
-from src.config import settings
 
 from src.schemas.users import UserRequestAdd, UserAdd
 from src.database import async_session_maker
 from src.repositories.users import UsersRepository
-
-
+from src.services.auth import AuthService
 
 
 router = APIRouter(prefix='/auth', tags=['Authenticate and Authorize'])
-
-pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
-SECRET_KEY = settings.SECRET_KEY
-ALGORITHM = settings.ALGORITHM
-ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
-
-
-def create_access_token(data: dict ) -> str:
-    to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode |= {"exp": expire} 
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
 
 
 @router.post('/register',
@@ -37,7 +15,7 @@ def verify_password(plain_password, hashed_password):
 async def register_user(
     data: UserRequestAdd
 ):
-    hashed_password = pwd_context.hash(data.password)
+    hashed_password = AuthService().hash_password(password=data.password)
     new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
     async with async_session_maker() as session:
         await UsersRepository(session).add(new_user_data)
@@ -53,11 +31,12 @@ async def login_user(data: UserRequestAdd,
                      response: Response):
     async with async_session_maker() as session:
         user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
+        auth_service = AuthService()
         if not user:
             raise HTTPException(status_code=401, detail='Incorrect user or password')
-        if not verify_password(data.password, user.hashed_password):
+        if not auth_service.verify_password(data.password, user.hashed_password):
             raise HTTPException(status_code=401, detail='Incorrect user or password')
-        access_token = create_access_token({'user_id': user.id})
+        access_token = auth_service.create_access_token({'user_id': user.id})
         response.set_cookie('access_token', access_token)
         return {'access_token': access_token}
     
